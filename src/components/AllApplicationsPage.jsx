58569ba7cc1f5add2bdf661a7d3bf27042f4dd3d
kharva <parvadesai404@gmail.com>
2025-03-22 01:47:15 +0530

    [Fix] headings

--- src/components/AllApplicationsPage.jsx
+++ src/components/AllApplicationsPage.jsx
@@ -3,22 +3,28 @@ import ApplicationsTable from "./ApplicationsTable.jsx";
 
 export default function AllApplicationsPage() {
   return (
-    <div class="app-data-container">
-      <table class="app-data-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Contact No</th>
-            <th>Course</th>
-            <th>Status</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <ApplicationsTable />
-        </tbody>
-      </table>
-    </div>
+    <section class="flex flex-col items-center justify-center gap-3 max-w-mdsm md:max-w-2xl lg:max-w-4xl 2xl:max-w-6xl">
+      <h2 class="text-center text-font-color-sec text-[34px] md:text-5xl font-extrabold mb-10">
+        Applications
+      </h2>
+
+      <div class="app-data-container">
+        <table class="app-data-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Contact No</th>
+              <th>Course</th>
+              <th>Status</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <ApplicationsTable />
+          </tbody>
+        </table>
+      </div>
+    </section>
   );
 }
--- src/components/IndApplicationPage.jsx
+++ src/components/IndApplicationPage.jsx
@@ -17,108 +17,114 @@ export default function ApplicationDetail({ id }) {
   }
 
   return (
-    <div class="ind-app-data-container">
-      <table class="ind-app-data-table">
-        <tbody id="applications-table">
-          <tr class="text-left">
-            <td colspan="2">
-              <img
-                id="passport-photo"
-                src="/images/examples/passport-photo.png"
-                alt="Passport Photo"
-                class="w-auto h-18"
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td>{application.full_name}</td>
-          </tr>
-          <tr>
-            <td>Age</td>
-            <td>{application.age}</td>
-          </tr>
-          <tr>
-            <td>DOB</td>
-            <td>{application.dob}</td>
-          </tr>
-          <tr>
-            <td>Gender</td>
-            <td>{application.gender}</td>
-          </tr>
-          <tr>
-            <td>Employment Status</td>
-            <td>{application.employment_status}</td>
-          </tr>
-          <tr>
-            <td>Occupation</td>
-            <td>{application.occupation}</td>
-          </tr>
+    <section class="flex flex-col items-center justify-center gap-3 max-w-mdsm md:max-w-2xl lg:max-w-4xl 2xl:max-w-6xl">
+      <h2 class="text-center text-font-color-sec text-[34px] md:text-5xl font-extrabold mb-10">
+        Application Details
+      </h2>
 
-          <tr>
-            <td>Mobile Number</td>
-            <td>{application.contact_no}</td>
-          </tr>
-          <tr>
-            <td>Email</td>
-            <td>{application.email}</td>
-          </tr>
-          <tr>
-            <td>Emergency Contact Number</td>
-            <td>{application.parent_contact_no}</td>
-          </tr>
-          <tr>
-            <td>Current Address</td>
-            <td>{application.curr_address}</td>
-          </tr>
-          <tr>
-            <td>Permanent Address</td>
-            <td>{application.per_address}</td>
-          </tr>
+      <div class="ind-app-data-container">
+        <table class="ind-app-data-table">
+          <tbody id="applications-table">
+            <tr class="text-left">
+              <td colspan="2">
+                <img
+                  id="passport-photo"
+                  src="/images/examples/passport-photo.png"
+                  alt="Passport Photo"
+                  class="w-auto h-18"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Name</td>
+              <td>{application.full_name}</td>
+            </tr>
+            <tr>
+              <td>Age</td>
+              <td>{application.age}</td>
+            </tr>
+            <tr>
+              <td>DOB</td>
+              <td>{application.dob}</td>
+            </tr>
+            <tr>
+              <td>Gender</td>
+              <td>{application.gender}</td>
+            </tr>
+            <tr>
+              <td>Employment Status</td>
+              <td>{application.employment_status}</td>
+            </tr>
+            <tr>
+              <td>Occupation</td>
+              <td>{application.occupation}</td>
+            </tr>
 
-          <tr>
-            <td>Course</td>
-            <td>{application.course}</td>
-          </tr>
-          <tr>
-            <td>Academic Qualificaion</td>
-            <td>{application.academic_qual}</td>
-          </tr>
-          <tr>
-            <td>Skills Relevant to the course</td>
-            <td>{application.skills}</td>
-          </tr>
-          <tr>
-            <td>Previous Training related to the similar course </td>
-            <td>{application.previous_training}</td>
-          </tr>
+            <tr>
+              <td>Mobile Number</td>
+              <td>{application.contact_no}</td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>{application.email}</td>
+            </tr>
+            <tr>
+              <td>Emergency Contact Number</td>
+              <td>{application.parent_contact_no}</td>
+            </tr>
+            <tr>
+              <td>Current Address</td>
+              <td>{application.curr_address}</td>
+            </tr>
+            <tr>
+              <td>Permanent Address</td>
+              <td>{application.per_address}</td>
+            </tr>
 
-          <tr>
-            <td>How did you hear about us?</td>
-            <td>{application.reference}</td>
-          </tr>
+            <tr>
+              <td>Course</td>
+              <td>{application.course}</td>
+            </tr>
+            <tr>
+              <td>Academic Qualificaion</td>
+              <td>{application.academic_qual}</td>
+            </tr>
+            <tr>
+              <td>Skills Relevant to the course</td>
+              <td>{application.skills}</td>
+            </tr>
+            <tr>
+              <td>Previous Training related to the similar course </td>
+              <td>{application.previous_training}</td>
+            </tr>
 
-          <tr>
-            <td colspan="2">
-              <a
-                id="view-aadhar-card"
-                class="form-submit-btn inline-block"
-                type="button"
-                aria-label="View Aadhar Card"
-                href="/images/examples/aadhar-card.pdf"
-                download="/images/examples/aadhar-card.pdf"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Aadhar Card
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      {/* <button onClick={() => route("/dashboard/applications")}>
+            <tr>
+              <td>How did you hear about us?</td>
+              <td>{application.reference}</td>
+            </tr>
+
+            <tr>
+              <td colspan="2">
+                <a
+                  id="view-aadhar-card"
+                  class="form-submit-btn inline-block"
+                  type="button"
+                  aria-label="View Aadhar Card"
+                  href="/images/examples/aadhar-card.pdf"
+                  download="/images/examples/aadhar-card.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Aadhar Card
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        {/* <button onClick={() => route("/dashboard/applications")}>
         Back to Applications
       </button> */}
-    </div>
+      </div>
+    </section>
   );
 }
